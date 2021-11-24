d96da30fc83cf0cd4b6d09c4ee2706cf4df372b3
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-24 09:42:05 -0300

    charpter 6 - avançando com rotas
    rotas
    nomeação de rotas
    rota de fallback
    grupo de rotas
    prefix
    
    encaminhamento de variaveis por controllers>view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function (){return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function (){
+    Route::get('/clientes', function (){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function (){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function (){return 'Produtos';})->name('app.produtos');
 });
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+//Route::get('/rota2', function (){
+//    return redirect()->route('site.rota1');
+//})->name('site.rota2');
+//Route::redirect('/rota2', '/rota1');
+
+Route::fallback(function (){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial';
+});
+
